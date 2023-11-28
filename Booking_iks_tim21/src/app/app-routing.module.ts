7c1b5537cb3dc21e8c113103e0ef09155cd121fd
Nesa02 <nenad.beric00@gmail.com>
2023-11-28 18:41:09 +0100

    navbar added

--- Booking_iks_tim21/src/app/app-routing.module.ts
+++ Booking_iks_tim21/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './layout/profile/profile.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'profile', component: ProfileComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Booking_iks_tim21/src/app/layout/layout.module.ts
+++ Booking_iks_tim21/src/app/layout/layout.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from '../infrastructure/material/material.module';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, ProfileComponent],
+  exports: [NavbarComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
 })
-export class LayoutModule { }
+export class LayoutModule {}

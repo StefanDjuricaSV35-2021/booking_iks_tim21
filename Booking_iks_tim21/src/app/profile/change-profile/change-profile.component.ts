0c07592a263b071611e8ee7f41ea67880d937b48
Nesa02 <nenad.beric00@gmail.com>
2023-12-05 21:37:09 +0100

    added connection to backend for change profile

--- Booking_iks_tim21/src/app/profile/change-profile/change-profile.component.ts
+++ Booking_iks_tim21/src/app/profile/change-profile/change-profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import { User } from '../model/user.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-change-profile',
   templateUrl: './change-profile.component.html',
-  styleUrls: ['./change-profile.component.css']
+  styleUrls: ['./change-profile.component.css'],
 })
-export class ChangeProfileComponent {
+export class ChangeProfileComponent implements OnInit {
+  user: User = {} as User;
 
+  constructor(private route: ActivatedRoute, private service: ProfileService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = +params['userId'];
+      //hardcoded to get user with id 1, for now.
+      this.service.getUser(1).subscribe({
+        next: (data: User) => {
+          this.user = data;
+        },
+      });
+    });
+  }
+
+  getPasswordStars(): string {
+    return '*'.repeat(this.user.password.length);
+  }
 }

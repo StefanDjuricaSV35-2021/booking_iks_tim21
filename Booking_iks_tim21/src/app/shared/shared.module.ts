add92b0dcc013d483fc05aaf4e4ad34616b55970
Borislav Čelar <bora.celar@gmail.com>
2023-12-16 01:35:46 +0100

    hotfix

--- Booking_iks_tim21/src/app/shared/shared.module.ts
+++ Booking_iks_tim21/src/app/shared/shared.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavbarComponent} from "../layout/navbar/navbar.component";
-import {FooterComponent} from "../layout/footer/footer.component";
-import {SignupComponent} from "../layout/signup/signup.component";
 import {SearchBarComponent} from "./components/search-bar/search-bar.component";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";

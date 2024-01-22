9a5485f0121e3a462ffe2a713bff31a5427ff597
StefanDjuricaSV35-2021 <stefandjurica420@gmail.com>
2024-01-22 19:59:58 +0100

    Added all the tests

--- Booking_iks_tim21/src/app/infrastructure/auth/signup/signup.component.spec.ts
+++ Booking_iks_tim21/src/app/infrastructure/auth/signup/signup.component.spec.ts
@@ -10,26 +10,10 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthModule } from '../auth.module';
 import { AuthService } from '../auth.service';
 import { of, throwError } from 'rxjs';
-import {Role, User} from "../../../core/models/user.model";
+import { Role, User } from "../../../core/models/user.model";
 import { NgZone } from '@angular/core';
-import {LoginComponent} from "../login/login.component";
-import { AppRoutingModule} from "../../../app-routing.module";
-
-function generateValidFormValues(): any {
-  return {
-    street: 'ValidStreet',
-    city: 'ValidCity',
-    country: 'ValidCountry',
-    name: 'ValidName',
-    surname: 'ValidSurname',
-    phone: '1234567890',
-    email: 'valid@example.com',
-    password: 'ValidPassword',
-    passwordRepeat: 'ValidPassword',
-    isOwner: true,
-  };
-}
-
+import { LoginComponent } from "../login/login.component";
+import { AppRoutingModule } from "../../../app-routing.module";
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -71,19 +55,20 @@ describe('SignupComponent', () => {
     expect(routerSpy).toHaveBeenCalledWith(['login']);
   });
 
-  it('should show error snackbar when signup fails', fakeAsync(() => {
+  it('should show error snackbar when signup fails', async () => {
+    fillFormWithValidValues(component);
+
     const signupError = 'Test signup error';
     spyOn(authService, 'signup').and.returnValue(throwError({ error: { message: signupError } }));
     spyOn(snackBar, 'open');
 
-    component.signUp();
-    tick();
+    await component.signUp();
 
     expect(snackBar.open).toHaveBeenCalledWith(signupError, 'Close', {
       verticalPosition: 'top',
       horizontalPosition: 'center',
     });
-  }));
+  });
 
   it('should show success snackbar when signup is successful', async () => {
     fillFormWithValidValues(component);
@@ -114,6 +99,8 @@ describe('SignupComponent', () => {
   });
 
   it('should show password mismatch error when passwords do not match', fakeAsync(() => {
+    fillFormWithValidValues(component);
+
     component.signUpForm.patchValue({
       password: 'password1',
       passwordRepeat: 'password2',
@@ -142,6 +129,183 @@ describe('SignupComponent', () => {
     });
   }));
 
+  it('should show error when street is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      street: '',
+    });
+
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when city is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      city: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when country is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      country: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when name is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      name: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when surname is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      surname: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when phone is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      phone: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when email is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      email: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when email does not match email validators', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      email: 'invalidEmail',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when password and password repeat is missing', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      password: '',
+      passwordRepeat: '',
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+  it('should show error when password and password repeat is shorter than 4 characters', fakeAsync(() => {
+    fillFormWithValidValues(component);
+    component.signUpForm.patchValue({
+      password: '123',
+      passwordRepeat: '123'
+    });
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
+
+  it('should send data stream of wrong cases and check form validity', fakeAsync(() => {
+    sendInvalidData();
+    spyOn(snackBar, 'open');
+
+    component.signUp();
+    tick();
+
+    expect(snackBar.open).toHaveBeenCalledWith("Some of your credentials aren't correct please check them again!!", 'Close', {
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  }));
+
   function fillFormWithValidValues(component: SignupComponent): void {
     component.signUpForm.setValue({
       street: 'ValidStreet',
@@ -156,4 +320,21 @@ describe('SignupComponent', () => {
       isOwner: true,
     });
   }
+
+  // Inside SignupComponent class
+  function sendInvalidData(): void {
+    component.signUpForm.setValue({
+      street: '',
+      city: '',
+      country: '',
+      name: '',
+      surname: '',
+      phone: '',
+      email: '',
+      password: '',
+      passwordRepeat: '',
+      isOwner: true,
+    });
+  }
+
 });
